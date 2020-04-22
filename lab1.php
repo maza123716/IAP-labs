60856c0978a7f0da9bd56f94f99160b26cf5f978
maza123716 <64093424+maza123716@users.noreply.github.com>
2020-04-22 11:39:59 +0300

    Add files via upload

---
+++ lab1.php
@@ -0,0 +1,42 @@
+<?php
+include_once 'DBConnector.php';
+include_once 'user.php';
+
+if (isset($_POST['btn-save'])){
+	$first_name = $_POST['first_name'];
+	$last_name = $_POST['last_name'];
+	$city = $_POST['city_name'];
+	
+	$user = new user($first_name,$last_name,$city);
+	$res = $user->save();
+	
+	if($res){
+		echo "Save operation was successful";
+	}else{
+		echo "An error occured!";
+	}
+}
+?>
+<html>
+    <head>
+	    <title>Form</title>
+	<head>
+	<body>
+	    <form method="post">
+		    <table align="center">
+			    <tr>
+				    <td><input type = "text" name="first_name" required placeholder="First Name" /></td>
+			    </tr>
+			    <tr>
+                    <td><input type = "text" name="last_name" placeholder = "Last Name" /></td>
+                </tr>
+                <tr>
+                    <td><input type = "text" name="city_name" placeholder = "City" /></td>
+                </tr>
+                <tr>
+                    <td><button type = "submit" name="btn-save"><strong>SAVE</strong><button></td>
+                </tr>
+            </table>				
+		</form>	
+	</body>	
+</html>	
